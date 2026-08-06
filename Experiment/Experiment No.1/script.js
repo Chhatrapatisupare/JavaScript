e35315c1a9029c0640c855ffe56f2f9f3b30e608
Chhatrapati Supare <chhatrapatisupare03@gmail.com>
2026-08-06 09:53:26 +0530

    Create script.js

---
+++ Experiment/Experiment No.1/script.js
@@ -0,0 +1,21 @@
+// Welcome Message
+alert("Welcome to SIT Nagpur CSE Department!");
+
+// Console Methods
+console.log("External JavaScript Loaded");
+console.info("Information Message");
+console.warn("Warning Message");
+console.error("Error Message Example");
+console.table([
+    {Name:"Chhatrapati Supare", RollNo:"24070621202"},
+    {Course:"B.Tech CSE", Semester:"5th"}
+]);
+
+// External JavaScript Function
+function showExternal()
+{
+    alert("External JavaScript Executed!");
+
+    console.log("External Button Clicked");
+    console.info("Student Information Displayed");
+}
